2b55353d2acd07c799bd39ea75862160fba9a186
Ravindu Deshan <prasannakumarasinha26@gmail.com>
2024-10-01 18:10:02 +0000

    added missing dtb flag

--- samsung/a05s/BoardConfig.mk
+++ samsung/a05s/BoardConfig.mk
@@ -46,6 +46,7 @@ TARGET_SCREEN_WIDTH := 2400
 
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
+TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb/dtb.dtb
 BOARD_KERNEL_CMDLINE := video=vfb:640x400,bpp=32,memsize=3072000 firmware_class.path=/vendor/firmware_mnt/image printk.devkmsg=on bootconfig androidboot.hardware=qcom androidboot.memcg=1 androidboot.load_modules_parallel=true androidboot.usbcontroller=4e00000.dwc3 loop.max_part=7 androidboot.selinux=permissive
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x02000000 --tags_offset 0x01e00000 --header_version 2 --board SRPWG06A004 --dtb=$(TARGET_PREBUILT_DTB)
 BOARD_KERNEL_PAGESIZE := 4096
