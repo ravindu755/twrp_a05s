962599f66e5a34e35860b09a2c3553eda4f13c70
Ravindu Deshan <prasannakumarasinha26@gmail.com>
2024-10-01 17:29:10 +0000

    added device specific values

--- samsung/a05s/device.mk
+++ samsung/a05s/device.mk
@@ -6,3 +6,17 @@
 #
 
 LOCAL_PATH := device/samsung/a05s
+
+# API levels
+PRODUCT_SHIPPING_API_LEVEL := 32
+
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 32
+
+# Dynamic
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.1-impl-mock \
+    fastbootd 
