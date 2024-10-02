788a9ab683d7d8a930c8c21dd9f4aca12b2f6400
Ravindu Deshan <prasannakumarasinha26@gmail.com>
2024-10-02 09:48:35 +0000

    remove useless line & TOUCH FINALLY WORKS..!

--- samsung/a05s/BoardConfig.mk
+++ samsung/a05s/BoardConfig.mk
@@ -35,7 +35,7 @@ TARGET_NO_BOOTLOADER := true
 
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_CMDLINE := video=vfb:640x400,bpp=32,memsize=3072000 firmware_class.path=/vendor/firmware_mnt/image printk.devkmsg=on bootconfig androidboot.hardware=qcom androidboot.memcg=1 androidboot.load_modules_parallel=true androidboot.usbcontroller=4e00000.dwc3 loop.max_part=7 androidboot.selinux=permissive msm_drm.dsi_display0=qcom,mdss_dsi_v2_ili7807s_tm_fhd_90hz_video ilitek.tpsensor=ILITEK_TMB_ILI7807S
+BOARD_KERNEL_CMDLINE := video=vfb:640x400,bpp=32,memsize=3072000 firmware_class.path=/vendor/firmware_mnt/image printk.devkmsg=on bootconfig androidboot.hardware=qcom androidboot.memcg=1 androidboot.load_modules_parallel=true androidboot.usbcontroller=4e00000.dwc3 loop.max_part=7 androidboot.selinux=permissive
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
