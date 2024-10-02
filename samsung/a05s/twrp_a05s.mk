e35c03760d570aa681449beff1a133b2fee7ff64
Ravindu Deshan <prasannakumarasinha26@gmail.com>
2024-10-02 03:26:19 +0000

    twrp specific changes
    using guide: https://github.com/ravindu644/Simple-Android-Guides/tree/twrp

--- samsung/a05s/twrp_a05s.mk
+++ samsung/a05s/twrp_a05s.mk
@@ -6,19 +6,18 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from a05s device
 $(call inherit-product, device/samsung/a05s/device.mk)
 
 PRODUCT_DEVICE := a05s
-PRODUCT_NAME := omni_a05s
+PRODUCT_NAME := twrp_a05s
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := SM-M145F
+PRODUCT_MODEL := Galaxy M14
 PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
